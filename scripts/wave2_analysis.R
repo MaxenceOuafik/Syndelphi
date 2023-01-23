537e36b8b3976578538bc0b81bbbaccf25b7c8a4
MaxenceOuafik <maxence.ouafik@outlook.com>
2023-01-23 12:44:15 +0100

    Simulation for different threshold values

--- scripts/wave2_analysis.R
+++ scripts/wave2_analysis.R
@@ -1,10 +1,12 @@
 source("./scripts/wave2_cleaning.R")
 
-# This function returns the name of the column wherever >= 80% of respondents have deemed the variable 
-# important or very important. It will be our sorting function. 
-select_variables <- function(x) {
+# This function returns the name of the column wherever a number of respondents 
+# equal or greather than the threshold have deemed the variable 
+# important or very important. It will be our sorting function. µ
+
+select_variables <- function(x, threshold) {
     if ((sum(x == "Important", na.rm = TRUE) + 
-    sum(x == "Très important", na.rm = TRUE)) >= (0.8 * nrow(x))) {
+    sum(x == "Très important", na.rm = TRUE)) >= (threshold * nrow(x))) {
         variablename <- colnames(x)
     } else {
        variablename <- NULL
@@ -12,21 +14,40 @@ select_variables <- function(x) {
    return(variablename)
 }
 
-# Then we will use a loop function to pass the sorting function we created and create a list
+# Then we will use a loop function to pass the sorting function we created and create a list for different threshold values
 variable_cisgender <- list()
 for (i in 1:45){
-    variable_cisgender[[i]] <- select_variables(wave2_data_cis[i])
+    variable_cisgender[["100%"]][[i]] <- select_variables(wave2_data_cis[i], 
+                                                         threshold = 1)
+    variable_cisgender[["80%"]][[i]] <- select_variables(wave2_data_cis[i], 
+                                                         threshold = 0.8)
+    variable_cisgender[["60%"]][[i]] <- select_variables(wave2_data_cis[i], 
+                                                         threshold = 0.6)
 }
 
 # To improve readibility, we will then remove null value to only keep the variables of interest
-variable_cisgender <- variable_cisgender[!sapply(variable_cisgender, is.null)]
-
+variable_cisgender[["100%"]] <- variable_cisgender[["100%"]][!sapply(variable_cisgender[["100%"]], 
+                                                                   is.null)]
+variable_cisgender[["80%"]] <- variable_cisgender[["80%"]][!sapply(variable_cisgender[["80%"]], 
+                                                                   is.null)]
+variable_cisgender[["60%"]] <- variable_cisgender[["60%"]][!sapply(variable_cisgender[["60%"]], 
+                                                                   is.null)]
 
 # We do the exact same thing for transgender respondents, keeping this time the 46th to 92nd columns
 
 variable_transgender <- list()
 for (i in 1:47){
-    variable_transgender[[i]] <- select_variables(wave2_data_trans[i])
+    variable_transgender[["100%"]][[i]] <- select_variables(wave2_data_trans[i], 
+                                                  threshold = 1)
+    variable_transgender[["80%"]][[i]] <- select_variables(wave2_data_trans[i], 
+                                                  threshold = 0.8)
+    variable_transgender[["60%"]][[i]] <- select_variables(wave2_data_trans[i], 
+                                                  threshold = 0.6)
 }
 
-variable_transgender <- variable_transgender[!sapply(variable_transgender, is.null)]
+variable_transgender[["100%"]] <- variable_transgender[["100%"]][!sapply(variable_transgender[["100%"]], 
+                                                                         is.null)]
+variable_transgender[["80%"]] <- variable_transgender[["80%"]][!sapply(variable_transgender[["80%"]], 
+                                                                         is.null)]
+variable_transgender[["60%"]] <- variable_transgender[["60%"]][!sapply(variable_transgender[["60%"]], 
+                                                                         is.null)]
